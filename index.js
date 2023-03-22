d6c46b59661b5121d48d78a1bfa0296e928a93cf
Trizzie Huynh <putmind2822@gmail.com>
2023-03-22 11:07:59 -0700

    added prompts and class SVG

--- index.js
+++ index.js
@@ -1,2 +1,55 @@
+const inquier = require("inquirer");
 
+inquier
+  .prompt([
+    {
+      type: "input",
+      name: "text",
+      message: "Enter up to three characters for the text",
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "Enter a color keyword for the text (OR a hexadecimal number)",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "Choose a shape for the logo",
+      choices: ["Circle", "Triangle", "Square"],
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "Enter a color keyword for the text (OR a hexadecimal number)",
+    },
+  ])
+  .then(({ text, textColor, shape, shapeColor }) => {
+    let userShape;
 
+    if (shape == "Circle") {
+      userShape = new Circle();
+    } else if (shape == "Triangle") {
+      userShape = new Triangle();
+    } else {
+      userShape = new Triangle();
+    }
+
+    userShape.setColor(shapeColor);
+
+    const svg = new SVG();
+    svg.setText(text, textColor);
+    svg.setShape(userShape);
+    return writeFile("logo.svg", svg.render());
+  })
+  .then(() => console.log("The logo is created."))
+  .catch((error)=>{
+    console.log(error);
+    console.log("Error!")
+  });
+
+  class SVG{
+    constructor(){
+      
+    }
+  }
